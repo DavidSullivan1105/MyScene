56a88661277b40ba697aea54243c4f43bfd0d576
David Sullivan <dcsulliv@gmail.com>
2022-11-14 22:28:08 -0500

    getbands()

--- MyScene.Services/BandService.cs
+++ MyScene.Services/BandService.cs
@@ -1,5 +1,6 @@
 ﻿using MyScene.Contracts;
 using MyScene.Data;
+using MyScene.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,38 @@ namespace MyScene.Services
         public BandService( ApplicationDbContext ctx)
         {
             _ctx = ctx;
+        }
+
+        public bool CreateBand(BandCreate model)
+        {
+            var entity =
+                new Band()
+                {
+                    OwnerId = _userId,
+                    BandName = model.BandName,
+                    BandGenre = model.Genre
+
+                };
+            _ctx.Bands.Add(entity);
+            return _ctx.SaveChanges() == 1;
+        }
+
+        public IEnumerable<BandListItem> GetBands()
+        {
+            var query =
+                _ctx
+                .Bands
+                .Where(e => e.OwnerId == _userId)
+                .Select(
+                    e =>
+                    new BandListItem
+                    {
+                        BandId = e.BandsId,
+                        BandName = e.BandName,
+                        Genre = e.BandGenre,
+
+                    });
+            return query.ToArray();
         }
 
         public void SetUserId(Guid userId)
